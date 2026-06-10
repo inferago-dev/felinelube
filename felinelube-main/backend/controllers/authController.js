283490e01924080bc85f077d3b7beda8275901d0
Mahesh-07-404 <mahesh52834@gmail.com>
2026-06-10 23:51:04 +0530

    fix: remove unused express-validator import causing MODULE_NOT_FOUND error on Render

--- felinelube-main/backend/controllers/authController.js
+++ felinelube-main/backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
+
 
 const prisma = new PrismaClient();
 
